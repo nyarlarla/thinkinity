7860a9b36290dd75ad13bd1290c9baa300ab9843
EC2 Default User <ec2-user@ip-172-31-45-46.ap-northeast-1.compute.internal>
2019-08-24 12:42:21 +0000

    デベロップ完了

--- db/migrate/20190731082102_create_idea_boards.rb
+++ db/migrate/20190731082102_create_idea_boards.rb
@@ -1,6 +1,6 @@
 class CreateIdeaBoards < ActiveRecord::Migration[5.2]
   def change
-    create_table :idea_boards, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC'  do |t|
+    create_table :idea_boards, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
       t.integer :user_id
       t.integer :tag_id
       t.integer :genre_id
